9a2cd9c5b0e29d93ccde92abc9a45ca9acf437a2
Radu Radu <neoradu@gmail.com>
2022-09-05 19:12:10 +0300

    it may be working with mysql, but i need to fix the DB init part
    Signed-off-by: Radu Radu <neoradu@gmail.com>

--- scripts/mysql-init.sql
+++ scripts/mysql-init.sql
@@ -1,21 +1,22 @@
 /*connect to mysql DB and run as root user*/
 
 /*Create databases:*/
-CREATE DATABASE recipe_app_dev;
-CREATE DATABASE recipe_app_prod;
+CREATE DATABASE recipe_app_dev_db;
+CREATE DATABASE recipe_app_prod_db;
 
 /*CREATE database users*/
-CREATE user 'recipe_app_dev_user@%' IDENTIFIED BY 'password';
-CREATE user 'recipe_app_prod_user@%' IDENTIFIED BY 'password';
+CREATE user 'recipe_app_dev_user'@'%' IDENTIFIED BY 'pass';
+CREATE user 'recipe_app_prod_user'@'%' IDENTIFIED BY 'pass';
 /*% --> wild card comming from any host*/
 
 /*Grant access to DML to databases*/
-GRANT SELECT ON recipe_app_dev.* to 'recipe_app_dev_user@%';
-GRANT INSERT ON recipe_app_dev.* to 'recipe_app_dev_user@%';
-GRANT DELETE ON recipe_app_dev.* to 'recipe_app_dev_user@%';
-GRANT UPDATE ON recipe_app_dev.* to 'recipe_app_dev_user@%';
+GRANT SELECT ON recipe_app_dev_db.* to 'recipe_app_dev_user'@'%';
+GRANT INSERT ON recipe_app_dev_db.* to 'recipe_app_dev_user'@'%';
+GRANT DELETE ON recipe_app_dev_db.* to 'recipe_app_dev_user'@'%';
+GRANT UPDATE ON recipe_app_dev_db.* to 'recipe_app_dev_user'@'%';
 
-GRANT SELECT ON recipe_app_prod.* to 'recipe_app_prod_user@%';
-GRANT INSERT ON recipe_app_prod.* to 'recipe_app_prod_user@%';
-GRANT DELETE ON recipe_app_prod.* to 'recipe_app_prod_user@%';
-GRANT UPDATE ON recipe_app_prod.* to 'recipe_app_prod_user@%';
+GRANT SELECT ON recipe_app_prod_db.* to 'recipe_app_prod_user'@'%';
+GRANT INSERT ON recipe_app_prod_db.* to 'recipe_app_prod_user'@'%';
+GRANT DELETE ON recipe_app_prod_db.* to 'recipe_app_prod_user'@'%';
+GRANT UPDATE ON recipe_app_prod_db.* to 'recipe_app_prod_user'@'%';
+
